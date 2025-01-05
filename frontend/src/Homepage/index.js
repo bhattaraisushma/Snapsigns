25d3a5118f4e56c8a835322b07866320b2b6c0f7
bhattaraisushma <bhattaraisushma260@gmail.com>
2025-01-05 20:30:58 +0545

    charcter removed

--- frontend/src/Homepage/index.js
+++ frontend/src/Homepage/index.js
@@ -4,10 +4,10 @@ import Character from '../Animated'
 const Homepage = () => {
   return (
     <div className='flex flex-col h-screen bg-purple-200 gap-5 justify-center items-center'>
-    
-      <Character/>
+{/*     
+      <Character/> */}
      
-    
+    <p className='text-6xl italic'>Show your <span className='text-purple-700'>creativity </span> here</p>
       <input type="text" placeholder="Enter text to generate sign language" className='border-solid  text-center h-[5rem] w-[20rem] rounded-[4rem]' />
     </div>
   )
