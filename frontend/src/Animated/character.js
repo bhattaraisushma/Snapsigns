bbe00540e9754f98c0f96218ae8a7075f408f97d
bhattaraisushma <bhattaraisushma260@gmail.com>
2025-01-22 19:25:18 +0545

    charcter position

--- frontend/src/Animated/character.js
+++ frontend/src/Animated/character.js
@@ -46,7 +46,7 @@ const Character = () => {
     };
   }, []);
 
-  return <div ref={mountRef} style={{ width: '100%', height: '100vh' }} />;
+  return <div ref={mountRef} style={{ width: '100%', height: '100vh' }}  />;
 };
 
 export default Character;
--- frontend/src/Homepage/index.js
+++ frontend/src/Homepage/index.js
@@ -2,15 +2,16 @@
 import React, { useState } from 'react';
 import WordDisplay from '../list';
 import WordToASLConverter from '../gloss';
-import Character from '../Animated/character';
+
 
 const Homepage = () => {
   const [selectedWord, setSelectedWord] = useState("");
 
   return (
     <div className='flex flex-row h-screen bg-purple-200 items-center'>
-      <Character/>
+     
       <div className='h-screen w-[40%]'>
+     
         <WordDisplay setSelectedWord={setSelectedWord} />
       </div>
       <WordToASLConverter selectedWord={selectedWord} setSelectedWord={setSelectedWord} />
