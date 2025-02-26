299df79a6ef68d08665994cd621ef85603715d65
bhattaraisushma <bhattaraisushma260@gmail.com>
2025-02-26 20:26:33 +0545

    nothing as such

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\Desktop\Backend for Snap\venv
+set VIRTUAL_ENV=D:\MINOR PROJECT\Snapsigns\Snapsigns\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
